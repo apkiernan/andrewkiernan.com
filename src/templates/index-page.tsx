071e456fa9d265c3476f27e2dc63b5a4fb7471b3
Andrew Kiernan <apkiernan@gmail.com>
2019-12-05 18:53:31 -0500

    Remove cruft from index

--- src/templates/index-page.tsx
+++ src/templates/index-page.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import { Link, graphql } from 'gatsby';
+import { graphql } from 'gatsby';
 
 import Layout from '../components/Layout';
-import BlogRoll from '../components/BlogRoll';
 import Helmet from 'react-helmet';
 
 type Img = {
@@ -33,40 +32,10 @@ export const IndexPageTemplate = ({ image, heading, description }: Props) => (
         backgroundRepeat: 'no-repeat'
       }}
     ></div>
-    <section className="section section--gradient">
-      <div className="container">
-        <div className="section">
-          <div className="columns">
-            <div className="column is-10 is-offset-1">
-              <div className="content">
-                <div className="content">
-                </div>
-                <div className="columns">
-                  <div className="column is-12">
-                    <h3 className="has-text-weight-semibold is-size-2">{heading}</h3>
-                    <p>{description}</p>
-                  </div>
-                </div>
-                <div className="columns">
-                  <div className="column is-12 has-text-centered">
-                    <Link className="btn" to="/products">
-                      See all products
-                    </Link>
-                  </div>
-                </div>
-                <div className="column is-12">
-                  <h3 className="has-text-weight-semibold is-size-2">Latest stories</h3>
-                  <BlogRoll />
-                  <div className="column is-12 has-text-centered">
-                    <Link className="btn" to="/blog">
-                      Read more
-                    </Link>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+    <section className="section section--gradient">      
+      <div className="column is-12">
+        <h3 className="has-text-weight-semibold is-size-2">{heading}</h3>
+        <p>{description}</p>
       </div>
     </section>
   </div>
