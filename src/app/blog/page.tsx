819df92e2530f43174efc7b6315815aa0c895c99
Andrew Kiernan <apkiernan@gmail.com>
2023-09-25 22:00:55 -0400

    Work on converting to inline markdown files

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,21 +1,60 @@
-import { getAllPosts } from '$lib/api';
-import { BlogPost } from './BlogPost';
+import fs from 'fs';
+import path from 'path';
+import { compiler } from 'markdown-to-jsx';
+import { GrayMatterFile, read } from 'gray-matter';
 
 export default async () => {
-	const { posts } = await getData();
+	const { posts } = await getPosts();
 	return (
 		<>
-			{posts.map(bp => (
-				<BlogPost post={bp} key={bp.slug} />
-			))}
+			{posts.map((bp) => {
+				return (
+					<>
+						<h1>{bp.data.title}</h1>
+						<p>{bp.data['date-created'].toISOString()}</p>
+						{compiler(bp.content)}
+					</>
+				);
+			})}
 		</>
 	);
 };
 
-async function getData() {
-	const { posts } = await getAllPosts();
+async function getPosts() {
+	const dirPath = path.resolve(process.cwd(), 'src', 'posts');
+	const filenames = await getFiles(dirPath);
 
-	return {
-		posts
-	};
+	const posts = await getPostContents(dirPath, filenames);
+
+	return { posts };
+}
+
+function getFiles(dirPath: string): Promise<string[]> {
+	return new Promise<string[]>((res, rej) => {
+		fs.readdir(dirPath, (err, files) => {
+			if (err) rej(err);
+
+			res(files);
+		});
+	});
+}
+
+function getPostContents(
+	dirPath: string,
+	files: string[]
+): Promise<GrayMatterFile<string>[]> {
+	return Promise.all(
+		files.map((file) => getPostContent(path.resolve(dirPath, file)))
+	);
+}
+
+function getPostContent(filePath: string): Promise<GrayMatterFile<string>> {
+	return new Promise<GrayMatterFile<string>>((res, rej) => {
+		try {
+			const content = read(filePath);
+			res(content);
+		} catch (err) {
+			rej(err);
+		}
+	});
 }
---
+++ src/posts/something-about-bun.md
@@ -0,0 +1,8 @@
+---
+date-created: 2020-02-20
+title: Bun Stuff
+---
+
+# Bun stuff
+
+Test content so that I can test reading and creating posts from markdown files
