cc6bc04e9ed8f1555841b56d6887dbd43e0628b9
Andrew Kiernan <apkiernan@gmail.com>
2023-09-30 19:02:38 -0400

    Fix contentful import

--- src/lib/contentful.ts
+++ src/lib/contentful.ts
@@ -1,15 +1,16 @@
-import { type ContentfulClientApi, createClient } from 'contentful';
+import contentful from 'contentful';
+
 import {
 	PUBLIC_CONTENTFUL_ACCESS_TOKEN,
 	PUBLIC_CONTENTFUL_HOST,
 	PUBLIC_CONTENTFUL_SPACE_ID
 } from '$env/static/public';
 
-let client: ContentfulClientApi<any>;
+let client: contentful.ContentfulClientApi<any>;
 
 export const getClient = () => {
 	if (!client) {
-		client = createClient({
+		client = contentful.createClient({
 			accessToken: PUBLIC_CONTENTFUL_ACCESS_TOKEN,
 			space: PUBLIC_CONTENTFUL_SPACE_ID,
 			host: PUBLIC_CONTENTFUL_HOST
